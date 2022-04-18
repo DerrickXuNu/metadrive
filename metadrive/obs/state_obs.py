316f546b5ea811ced3242bbfa8de8b9148924f8c
lqy0057 <785878978@qq.com>
2022-04-18 11:36:53 +0800

    modify obs

--- metadrive/obs/state_obs.py
+++ metadrive/obs/state_obs.py
@@ -115,13 +115,13 @@ class StateObservation(ObservationBase):
             # then add the cloud points of lane line detector
             info += vehicle.lane_line_detector.perceive(vehicle, vehicle.engine.physics_world.static_world).cloud_points
 
-        else:
-
-            # If the lane line detector is turn off, then add the offset of current position
-            # against the central of current lane to the state. If vehicle is centered in the lane, then the offset
-            # is 0 and vice versa.
-            _, lateral = vehicle.lane.local_coordinates(vehicle.position)
-            info.append(clip((lateral * 2 / vehicle.navigation.map.MAX_LANE_WIDTH + 1.0) / 2.0, 0.0, 1.0))
+        # else:
+        #
+        #     # If the lane line detector is turn off, then add the offset of current position
+        #     # against the central of current lane to the state. If vehicle is centered in the lane, then the offset
+        #     # is 0 and vice versa.
+        #     _, lateral = vehicle.lane.local_coordinates(vehicle.position)
+        #     info.append(clip((lateral * 2 / vehicle.navigation.map.MAX_LANE_WIDTH + 1.0) / 2.0, 0.0, 1.0))
 
         return info
 
@@ -129,7 +129,7 @@ class StateObservation(ObservationBase):
         dim = 0
         dim += 2 if self.config["side_detector"]["num_lasers"] == 0 else \
             self.config["side_detector"]["num_lasers"]
-        dim += 1 if self.config["lane_line_detector"]["num_lasers"] == 0 else \
+        dim += 0 if self.config["lane_line_detector"]["num_lasers"] == 0 else \
             self.config["lane_line_detector"]["num_lasers"]
         return dim
 
